d6a321fd97fdb291e195baac871a575577989f1a
devhiko <aydin96tr@gmail.com>
2022-06-26 18:41:17 +0300

    code edited

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -39,9 +39,7 @@ export const getStaticProps = async ({ params }) => {
 
 export default function RecipeDetails({ recipe }) {
 
-  if (!recipe) {
-    return (<Skeleton />)
-  }
+  if (!recipe) return (<Skeleton />)
 
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
   const { url } = featuredImage.fields.file
